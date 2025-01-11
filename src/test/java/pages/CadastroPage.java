693f0cc3e07b7b1b3261a9d4e3737e234b42b290
Administrador <lucascamargo@gmail.com>
2025-01-11 14:17:35 -0300

    Oitavo commit / 11.01.2025

--- src/test/java/pages/CadastroPage.java
+++ src/test/java/pages/CadastroPage.java
@@ -1,4 +1,85 @@
 package pages;
 
-public class CadastroPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import sun.nio.ch.Util;
+import support.Utils;
+
+public class CadastroPage extends Utils {
+
+    WebDriver driver;
+
+    private By titleM = By.id("id_gender1");
+    private By titleF = By.id("id_gender2");
+    private By fisrt_name_field = By.id("custumer_fisrtname");
+    private By last_name_field = By.id("custumer_lastname");
+    private By password_field = By.id("passwd");
+
+    private By select_day_field = By.id("days");
+    private By select_months_field = By.id("months");
+    private By select_years_field = By.id("years");
+
+    private By address_field = By.id("address1");
+    private By city_field = By.id("city");
+    private By state_field = By.id("id_state");
+    private By postcode_field = By.id("post_code");
+    private By phone_mobile_field = By.id("phone_mobile");
+
+    public CadastroPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void selectTitle(Integer type){
+        waitElementBePresent(titleM, 20);
+        if(type ==1) {
+            driver.findElement(titleM).click();
+        }else if(type ==2){
+            driver.findElement(titleF).click();
+        }
+    }
+
+    public void preencheNome(String name){
+        driver.findElement(fisrt_name_field).sendKeys(name);
+    }
+
+    public void preencheSobrenome(String sobrenome){
+        driver.findElement(last_name_field).sendKeys(sobrenome);
+    }
+
+    public void preenchePassword(String pass){
+        driver.findElement(password_field).sendKeys(pass);
+    }
+
+    public void selecionaAniversario(Integer day, Integer months, String year){
+        Select select_day = new Select(driver.findElement(select_day_field));
+        select_day.selectByIndex(day);
+
+        Select select_month = new Select(driver.findElement(select_months_field));
+        select_month.selectByIndex(months);
+
+        Select select_year = new Select(driver.findElement(select_years_field));
+        select_year.selectByValue(year);
+    }
+
+    public void preencheAddress(String address){
+        driver.findElement(address_field).sendKeys(address);
+    }
+
+    public void preencheCity(String city){
+        driver.findElement(city_field).sendKeys(city);
+    }
+
+    public void selecionaState(String state){
+        Select select_state = new Select(driver.findElement(state_field));
+        select_state.selectByVisibleText(state);
+    }
+
+    public void preenchePostCode(String postcode){
+        driver.findElement(postcode_field).sendKeys(postcode);
+    }
+
+    public void preenchePhoneMobile(String phone){
+        driver.findElement(phone_mobile_field).sendKeys(phone);
+    }
 }
--- src/test/java/steps/CadastroSteps.java
+++ src/test/java/steps/CadastroSteps.java
@@ -1,17 +1,27 @@
 package steps;
 
-import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import pages.CadastroPage;
 import runner.RunCucumberTest;
 
 public class CadastroSteps extends RunCucumberTest {
 
 
+    CadastroPage cadastroPage = new CadastroPage(driver);
 
     @Quando("^eu preencho o formlulário de cadastro$")
     public void eu_preencho_o_formlulário_de_cadastro() {
-
+        cadastroPage.selectTitle(1);
+        cadastroPage.preencheNome("Lucas");
+        cadastroPage.preencheSobrenome("Camargo");
+        cadastroPage.preenchePassword("@fnjf595");
+        cadastroPage.selecionaAniversario(30,9, "1998");
+        cadastroPage.preencheAddress("Rua teste, 123");
+        cadastroPage.preencheCity("Florianópolis");
+        cadastroPage.selecionaState("Colorado");
+        cadastroPage.preenchePostCode("987788989");
+        cadastroPage.preenchePhoneMobile("+55045985548373");
     }
 
     @Quando("^clico em registrar$")
